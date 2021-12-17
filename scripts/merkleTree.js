64033b36024ce67b0b18ed5596a58dcd46493340
straightupjac <jdc.jaclyn@gmail.com>
2021-12-17 01:48:29 -0600

    fix merkleTree.js

--- scripts/merkleTree.js
+++ scripts/merkleTree.js
@@ -1,16 +1,16 @@
 const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256');
 
-const elfAddresses = require('../whitelist/elf-address.json');
-const reindeerAddresses = require('../whitelist/reindeer-address.json')
+const elfAddresses = require('../whitelist/elfAddress.json');
+const reindeerAddresses = require('../whitelist/reindeerAddress.json')
 
 const elfLeafNodes = elfAddresses.map((addr) => keccak256(addr));
 const elfMerkleTree = new MerkleTree(elfLeafNodes, keccak256, { sortPairs: true });
 const elfRootHash = elfMerkleTree.getRoot();
-console.log('elfMerkleRoot', elfRootHash.toString());
+console.log('elfMerkleRoot', elfRootHash.toString('hex'));
 
 const reindeerLeafNodes = reindeerAddresses.map((addr) => keccak256(addr));
 const reindeerMerkleTree = new MerkleTree(reindeerLeafNodes, keccak256, { sortPairs: true });
 const reindeerRootHash = reindeerMerkleTree.getRoot();
 
-console.log('reindeerMerkleRoot', reindeerRootHash.toString());
+console.log('reindeerMerkleRoot', reindeerRootHash.toString('hex'));
